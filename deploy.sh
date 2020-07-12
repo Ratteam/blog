68876d74689e839febf4231a61eaf614811cf5a4
xuyanan <1548582000@qq.com>
2020-07-12 08:46:59 +0800

    7

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,19 @@
-# 自动部署脚本  
+#!/usr/bin/env sh
 
-# 构建
-yarn docs:build
-# 导航到构建输出目录
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 第一次使用
 git init
 git add -A
 git commit -m 'deploy'
 
 # 推到你仓库的 master 分支
 git push -f https://github.com/RatFrog/ratfrog.github.io.git master
+
+cd -
